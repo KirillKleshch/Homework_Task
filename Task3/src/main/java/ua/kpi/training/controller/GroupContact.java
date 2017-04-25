8d14c0e55c2e763609d9a887fa24f966a11d609f
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-04-26 00:44:26 +0300

    Изменил перечесление групп в которые может быть занесен абонент

--- Task3/src/main/java/ua/kpi/training/controller/GroupContact.java
+++ Task3/src/main/java/ua/kpi/training/controller/GroupContact.java
@@ -4,14 +4,14 @@ package ua.kpi.training.controller;
  * Created by Kirill on 20.04.2017.
  */
 public enum GroupContact {
-    FRIENDS("friends"),
-    FAMILY("family"),
-    WORK("work"),
-    OTHER("other");
+    FRIENDS("Друзья"),
+    FAMILY("Семья"),
+    WORK("Работа"),
+    OTHER("Другое");
 
     private String groupName;
 
-    GroupContact(String groupName){
+    GroupContact(String groupName) {
         this.groupName = groupName;
     }
 
