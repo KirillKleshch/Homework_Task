599a307324be2ce93a5cfa4d7b49b45238634345
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-04-26 00:42:10 +0300

    Переделал регулярные выражения

--- Task3/src/main/java/ua/kpi/training/controller/RegExp.java
+++ Task3/src/main/java/ua/kpi/training/controller/RegExp.java
@@ -5,15 +5,17 @@ package ua.kpi.training.controller;
  */
 public interface RegExp {
     String NAME_REGEXP = "^[A-Z][a-z]{2,30}$";
+    String AGE_REGEXP = "^((1[012][0-9])|([1-9][0-9]))$";
     String EMAIL_REGEXP = "^[A-z0-9_.]{1,30}@([A-z]+[A-z0-9]{1,15}.){1,2}([A-z]+[A-z0-9]{1,15})$";
     String CELL_PHONE_REGEXP = "([\\+]\\d{2}\\s?)?(([\\(\\s]\\d{3}[\\)\\s])|" +
             "(\\d{3}))[\\s-]?\\d{3}[\\s-]?\\d{2}[\\s-]?\\d{2}$";
     String CELL_PHONE_OPTIONAL_REGEXP = "^(([\\+]\\d{2}\\s?)?(([\\(\\s]\\d{3}[\\)\\s])|" +
             "(\\d{3}))[\\s-]?\\d{3}[\\s-]?\\d{2}[\\s-]?\\d{2})?";
     String LOCAL_PHONE_REGEXP = "^([\\+]\\d{2}\\s?)?(([\\(\\s]\\d{3}[\\)\\s])|(\\d{3}))?[\\s-]?\\d{3}[\\s-]?\\d{3}$";
-    String AVERAGE_TEXT_REGEXP = "^([\\d\\s\\w\\.\\,\\!]){0,127}$";
-    String GROUP_REGEXP = "^[\\w]{4,10}$";
+    String COMMENT_REGEXP = "^([\\d\\s\\w\\.\\,\\!]){0,127}$";
+    String GROUP_REGEXP = "^[A-z]|[А-я]{4,10}$";
     String SKYPE_NICK_REGEXP = "^[\\w\\d\\_]{3,20}$";
+    String INDEX_REGEXP = "^[\\d]{8}$";
     String CITY_STREET_REGEXP = "^[A-Z][a-z]{3,20}$";
     String BUILDING_REGEXP = "^[\\d]{1,3}[\\w]?$";
     String FLAT_REGEXP = "^[\\d]{1,3}$";
