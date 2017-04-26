d1e41228a8fff6f8306f06d95b87e21f3768a24d
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-04-26 15:14:29 +0300

    Сделал свое исключение

---
+++ Task3/src/main/java/ua/kpi/training/model/ReMeetingNickNameException.java
@@ -0,0 +1,10 @@
+package ua.kpi.training.model;
+
+/**
+ * Created by Kirill on 25.04.2017.
+ */
+public class ReMeetingNickNameException extends Exception{
+    public ReMeetingNickNameException(String message){
+        super(message);
+    }
+}
