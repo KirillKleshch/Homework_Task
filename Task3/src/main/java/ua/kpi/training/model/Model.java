2447af271b9a727cb95a36087ea3e8c395ae9905
Kirill Kleshch <kirill.pochta.kpi@gmail.com>
2017-04-26 15:16:46 +0300

    Изменил метод добавления контакта, тепреь он бросает исключение

--- Task3/src/main/java/ua/kpi/training/model/Model.java
+++ Task3/src/main/java/ua/kpi/training/model/Model.java
@@ -1,6 +1,7 @@
 package ua.kpi.training.model;
 
 import ua.kpi.training.controller.Notebook;
+import ua.kpi.training.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +14,12 @@ public class Model {
 
     private List<Notebook> noteBook = new ArrayList<>();
 
-    public void addContact(Notebook newContact) {
-        //newContact.getNickName()
+    public void addContact(Notebook newContact) throws ReMeetingNickNameException {
+        for(NickName nick : NickName.values()){
+            if(newContact.getNickName().equalsIgnoreCase(nick.getDescription())){
+                throw new ReMeetingNickNameException(View.ERROR_NICK);
+            }
+        }
         noteBook.add(newContact);
     }
 
